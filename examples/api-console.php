ab463973852e0de67e93390071078ba0a1192745
Oren yagev <oyagev@oren.(none)>
2010-07-20 17:10:48 +0300

    Add API Console Page for New Projects

--- examples/api-console.php
+++ examples/api-console.php
@@ -3,16 +3,17 @@
 	$prod = 'https://www.onehourtranslation.com';
 	
 
-	$account_id = 269;
-	$secret_key = '5a68561984276108fa42d7cffbbf91a5';
+	$account_id = 269; //demo user
+	$secret_key = '5a68561984276108fa42d7cffbbf91a5'; //demo user
 	$url = $sandbox;
 
 ?>
+<h1>OneHourTranslation API Test Console</h1>
 <div>Submit a New Project:</div>
 <form method="post" action="<?php echo $url?>/api/1/project/new/" target="resIframe">
 	URL: <input type="text" name="url" value="<?php echo $url?>" readonly="readonly" disabled="disabled"/> <br />
-	Account ID: <input type="text" name="account_id" value="<?php echo $account_id?>" readonly="readonly"/> <br />
-	Secret Key: <input type="text" name="secret_key" value="<?php echo $secret_key?>" readonly="readonly" /><br />
+	Account ID: <input type="text" name="account_id" value="<?php echo $account_id?>" /> <br />
+	Secret Key: <input type="text" name="secret_key" value="<?php echo $secret_key?>"  /><br />
 	Source: <input type="text" name="source" /><br />
 	Target: <input type="text" name="target" /><br />
 	Content: <textarea name="content" cols="50" rows="20"></textarea><br />
